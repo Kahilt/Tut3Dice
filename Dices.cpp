026b8323b71b344d11f7cda0816e36affbf6559e
Kahilt <kahilt11@gmail.com>
2016-03-06 10:13:07 +0200

    Seeding random number

--- Dices.cpp
+++ Dices.cpp
@@ -20,8 +20,10 @@ Dice::Dice() :num(0)
 
 void Dice::Roll(int number)
 {
+	number = rand() % 6 + 1; // For random number generation between 1 and 6
 }
 
 int main()
 {
+	srand(time(NULL)); // For random number generation
 }
